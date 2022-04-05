338b9dd3d715828cefdf44808c53fd8d6eeb4562
Andrey Shabunov <shabunovaa@yandex.ru>
2022-04-05 17:36:41 +0300

    05.04.2022 Алгоритм Бинарного поиска (Binary Search) | JavaScript Объяснение функции

--- Materials/JavaScript/Exercises/BinarySearch.js
+++ Materials/JavaScript/Exercises/BinarySearch.js
@@ -8,23 +8,23 @@
 // Example #1
 let array = [-1, 0, 3, 5, 7, 9, 12,];
 
-let search = function(nums, target) {
-    let left = 0;
-    let right = nums.length -1;
-    let mid;
+let search = function(nums, target) { // Функция, которая в качестве аргумента получает отсортированный массив с числами и target, который мы ищем
+    let left = 0; // левая граница
+    let right = nums.length -1; // правая граница
+    let mid; // середина массива
 
-    while (left <= right) {
-        mid = Math.round((right-left)/2) + left
+    while (left <= right) { // итерироваться до тех пор пока левая граница меньше или равна правой
+        mid = Math.round((right-left)/2) + left // Math.round() - так как индексы целочисленные. Вычисляем длину текущего подмассива. Расчет позиции центрального элемента
 
-        if (target === nums[mid]) {
-            return mid;
-        } else if (target < nums[mid]) {
-            right = mid - 1;
-        } else {
+        if (target === nums[mid]) { // если число равно числу, которое лежит в nums в центральной позиции
+            return mid; // возвращаем позицию искомого элемента (нашли это число)
+        } else if (target < nums[mid]) { // если число меньше чем середина то всю правую часть можно откинуть
+            right = mid - 1; // поменяли позицию правого указателя
+        } else { // число больше чем nums[mid] - меняем позицию левого указателя
             left = mid + 1;
         }
     }
-    return  - 1;
+    return  - 1; // возвращаем "-1" если искомого элемента в массиве не оказалось
 }
 
-console.log(search(array, 12))
+console.log(search(array, 12)) // 6
