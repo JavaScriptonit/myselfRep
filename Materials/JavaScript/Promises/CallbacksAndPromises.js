9b3ed24dd8b75d9604303a729482d4d0bf05103e
Andrey Shabunov <shabunovaa@yandex.ru>
2022-04-12 18:25:44 +0300

    12.04.2022 Promises and Callbacks

--- Materials/JavaScript/Promises/CallbacksAndPromises.js
+++ Materials/JavaScript/Promises/CallbacksAndPromises.js
@@ -3,6 +3,8 @@
 // Удобно использовать для отображения ошибки Пользователю после длительного процесса (например скачивания файла),
 // Если файл не был скачен - отобразить ошибку reject Promise
 
+// Example 1 (new Promise)
+
 let p = new Promise((resolve, reject) => { // у Promise есть 2 результата: если Promise успешно закончен или failed
     let a = 1 + 1  // сам Promise, если он будет зафейлен - функция выполняет reject часть
     if (a === 2) { //
@@ -13,15 +15,15 @@ let p = new Promise((resolve, reject) => { // у Promise есть 2 резуль
 })
 
 p.then((message) => { // в .then ловим ответ при resolve Promise
-    // console.log('This is in the then ' + message) // выводим в консоль если Success
+    console.log(1, 'This is in the then ' + message) // выводим в консоль если Success
 }).catch((message) => { // только в .catch() можем поймать (catch error) reject Promise
-    // console.log('This is in the catch ' + message) // выводим в консоль если Fail
+    console.log(1, 'This is in the catch ' + message) // выводим в консоль если Fail
 })
 
 const userLeft = false;
 const userWatchingCatMeme = false;
 
-// Example 1 (with Callbacks)
+// Example 2 (Function with Callbacks)
 
 function watchTutorialCallback(callback, errorCallback) {
     if (userLeft) {
@@ -40,12 +42,12 @@ function watchTutorialCallback(callback, errorCallback) {
 }
 
 watchTutorialCallback((message) => { // у функции 2 колбэка: для success и error
-    console.log('Success: ' + message)
+    console.log(2, 'Success: ' + message)
 }, (error) => {
-    console.log(error.name + ' ' + error.message)
+    console.log(2, error.name + ' ' + error.message)
 })
 
-// Example 2 (with Promise)
+// Example 3 (Function with Promise)
 
 function watchTutorialPromise() {
     return new Promise((resolve, reject) => {
@@ -66,7 +68,7 @@ function watchTutorialPromise() {
 }
 
 watchTutorialPromise().then((message) => { // у функции нет параметров. При resolve выводит в консоль message, а при reject'ах выводит из объекта name и message
-    console.log('Success: ' + message)
+    console.log(3, 'Success: ' + message)
 }).catch((error) => {
-    console.log(error.name + ' ' + error.message)
+    console.log(3, error.name + ' ' + error.message)
 })
