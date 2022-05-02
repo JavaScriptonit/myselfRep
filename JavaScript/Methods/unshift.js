70142a4b270b34dd16054eb8bb58b3fa1a79a743
Andrey Shabunov <shabunovaa@yandex.ru>
2022-05-02 23:54:53 +0300

    02.05.2022 unshift() method

---
+++ JavaScript/Methods/unshift.js
@@ -0,0 +1,14 @@
+// Добавление или удаления элемента из массива (unshift() method):
+
+// Example #1
+// const CountriesArray = await page.evaluate((selectors) => {
+//     const country = document.querySelectorAll(selectors.registrationWindow.countryNameList) // '.lliPHg'
+//     const firstCountryFromList = document.querySelector('.gkxekH').innerText // '.gkxekH' // Россия в списке стран (1ая страна)
+//     const countriesArray = Array.from(country)
+//     const countriesArrayText = countriesArray.map(e => e.innerText)
+//     countriesArrayText.unshift(firstCountryFromList) // unshift() method is used to add one or more elements to the beginning of the given array.
+//     return countriesArrayText
+// }, selectors)
+
+// Example #2
+Array.prototype.unshift()
