51a7e9bf536a187c091ea190025be8cedf21faba
Andrey Shabunov <shabunovaa@yandex.ru>
2022-05-17 19:39:52 +0300

    17.05.2022 calculator.js update package.json

--- JavaScript/Frameworks/Cypress/cypress/integration/tests/calculator.js
+++ JavaScript/Frameworks/Cypress/cypress/integration/tests/calculator.js
@@ -1,5 +1,15 @@
 describe('anonymous calculator', () => {
-    it('can make calculations', () => {
-
+    it('can make calculation', () => {
+        cy.visit('http://localhost:8080')
+            .get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(3)') // get 1
+            .click()
+            .get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(4)') // get +
+            .click()
+            .get('._2S_Gj6clvtEi-dZqCLelKb > :nth-child(5)') // get 3
+            .click()
+            .get('._1yUJ9HTWYf2v-MMhAEVCAn > :nth-child(5)') // get =
+            .click()
+            .get('.mNQM6vIr72uG0YPP56ow5')// get display
+            .should('have.text', '4');
     })
 })
