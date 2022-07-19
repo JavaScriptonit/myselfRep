f2050ffe749405447bdf81a1624a8a9ba576ce0e
Andrey Shabunov <shabunovaa@yandex.ru>
2022-07-19 11:42:39 +0300

    interview#1.md part 3 + примеры решений задач + термины

--- Interviews/QA-Automation/JavaScript/index.js
+++ Interviews/QA-Automation/JavaScript/index.js
@@ -19,12 +19,13 @@ const myArray = [{
     "salary": 1600,
 }];
 
+// 1ый вариант решения задач:
 const getNames = () => {
     let arrayCharname = [];
     myArray.forEach((object) => {
         arrayCharname.push(object.charname + ' ' + object.surname);
     });
-    console.log('Example #1', arrayCharname);
+    console.log('Example #1', arrayCharname); // [ 'John Rose', 'Margo Rose', 'Lisa Barcley' ]
 };
 
 getNames();
@@ -35,7 +36,7 @@ const getTotalSalary = () => {
         arraySalary.push(object.salary);
     });
     let totalSalary = arraySalary[0] + arraySalary[1] + arraySalary[2];
-    console.log('Example #2', totalSalary);
+    console.log('Example #2', totalSalary); // 2900
 };
 
 getTotalSalary();
@@ -47,7 +48,43 @@ const getAllRoseSurname = () => {
             arraySurname.push(object.charname);
         }
     });
-    console.log('Example #3', arraySurname);
+    console.log('Example #3', arraySurname); // [ 'John', 'Margo' ]
 };
 
 getAllRoseSurname();
+
+// 2ой вариант решения задач:
+const getNamesShortcut = (arr) => {
+    console.log('Example #1', arr.map(el => `${el.surname} ${el.charname}`)); // [ 'Rose John', 'Rose Margo', 'Barcley Lisa' ]
+    return arr.map(el => `${el.surname} ${el.charname}`);
+};
+
+getNamesShortcut(myArray);
+
+const getTotalSalaryShortcut = (arr) => {
+    console.log('Example #2', arr.reduce((acc, curr) => acc + curr.salary, 0)); // 2900
+    return arr.reduce((prev, curr) => prev + curr.salary, 0);
+};
+
+getTotalSalaryShortcut(myArray);
+
+const getAllRoseSurnameShortcut = (arr) => {
+    console.log('Example #3', arr.filter(object => object.surname === 'Rose').map(el => el.charname)); // [ 'John', 'Margo' ]
+    return arr.filter(object => object.surname === 'Rose').map(el => el.charname);
+};
+
+getAllRoseSurnameShortcut(myArray);
+
+// number of unique surnames:
+function uniqueSurname(arr) {
+    let unique = new Set();
+    arr.map(el => unique.add(el.surname));
+    console.log('Example #4', unique.size); // 2
+    return unique;
+}
+
+uniqueSurname(myArray);
+
+// The Set object lets you store unique values of any type, whether primitive values or object references.
+// Set objects are collections of values. A value in the Set may only occur once; it is unique in the Set's collection.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
